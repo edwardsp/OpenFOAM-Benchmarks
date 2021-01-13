216d5698723d7cd000ee42b81fd5c537f63721e1
edwardsp <paul.m.edwards@gmail.com>
2021-01-13 16:59:40 +0000

    Update param_sweep.sh
    typo

--- param_sweep.sh
+++ param_sweep.sh
@@ -2,11 +2,11 @@
 
 # hbv2
 run_sweep.sh hbv2_drivaer_s "220 20 32" "30 60 90 120" "1 2 4 8 16" 120 15
-run_sweep.sh hbv2_drivaer_s "440 40 64" "30 60 90 120" "1 2 4 8 16" 120 15
-run_sweep.sh hbv2_drivaer_s "660 60 96" "30 60 90 120" "1 2 4 8 16" 120 15
+run_sweep.sh hbv2_drivaer_m "440 40 64" "30 60 90 120" "1 2 4 8 16" 120 15
+run_sweep.sh hbv2_drivaer_l "660 60 96" "30 60 90 120" "1 2 4 8 16" 120 15
 
 # hc
-run_sweep.sh hbv2_drivaer_s "220 20 32" "30 60 90 120" "1 2 4 8 16" 44 2
-run_sweep.sh hbv2_drivaer_s "440 40 64" "30 60 90 120" "1 2 4 8 16" 44 2
-run_sweep.sh hbv2_drivaer_s "660 60 96" "30 60 90 120" "1 2 4 8 16" 44 2
+run_sweep.sh hc_drivaer_s "220 20 32" "16 22 32 44" "1 2 4 8 16" 44 2
+run_sweep.sh hc_drivaer_m "440 40 64" "16 22 32 44" "1 2 4 8 16" 44 2
+run_sweep.sh hc_drivaer_l "660 60 96" "16 22 32 44" "1 2 4 8 16" 44 2
 
