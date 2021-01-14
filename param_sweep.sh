6c6c0c912eb5d407a854d2bf356d7f2c57fe55b0
edwardsp <paul.m.edwards@gmail.com>
2021-01-14 10:11:53 +0000

    correct numa domains for hbv2

--- param_sweep.sh
+++ param_sweep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # hbv2
-run_sweep.sh hbv2_drivaer_s "220 20 32" "30 60 90 120" "1 2 4 8 16" 120 15
-run_sweep.sh hbv2_drivaer_m "440 40 64" "30 60 90 120" "1 2 4 8 16" 120 15
-run_sweep.sh hbv2_drivaer_l "660 60 96" "30 60 90 120" "1 2 4 8 16" 120 15
+run_sweep.sh hbv2_drivaer_s "220 20 32" "30 60 90 120" "1 2 4 8 16" 120 30
+run_sweep.sh hbv2_drivaer_m "440 40 64" "30 60 90 120" "1 2 4 8 16" 120 30
+run_sweep.sh hbv2_drivaer_l "660 60 96" "30 60 90 120" "1 2 4 8 16" 120 30
 
 # hc
 run_sweep.sh hc_drivaer_s "220 20 32" "16 22 32 44" "1 2 4 8 16" 44 2
