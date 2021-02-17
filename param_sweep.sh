2a3be981d7f6884333f3af141d7e107ea0529ea6
Paul Edwards <paul.m.edwards@gmail.com>
2021-02-17 18:21:08 +0000

    updated to add slot_type in the qsub

--- param_sweep.sh
+++ param_sweep.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # hbv2
-run_sweep.sh hbv2_drivaer_s "220 20 32" "30 60 90 120" "1 2 4 8 16" 120 15
-run_sweep.sh hbv2_drivaer_m "440 40 64" "30 60 90 120" "1 2 4 8 16" 120 15
-run_sweep.sh hbv2_drivaer_l "660 60 96" "30 60 90 120" "1 2 4 8 16" 120 15
+run_sweep.sh hbv2_drivaer_s "220 20 32" "30 60 90 120" "1 2 4 8 16" 120 15 hb120rs_v2
+run_sweep.sh hbv2_drivaer_m "440 40 64" "30 60 90 120" "1 2 4 8 16" 120 15 hb120rs_v2
+run_sweep.sh hbv2_drivaer_l "660 60 96" "30 60 90 120" "1 2 4 8 16" 120 15 hb120rs_v2
 
 # hc
-run_sweep.sh hc_drivaer_s "220 20 32" "16 22 32 44" "1 2 4 8 16" 44 2
-run_sweep.sh hc_drivaer_m "440 40 64" "16 22 32 44" "1 2 4 8 16" 44 2
-run_sweep.sh hc_drivaer_l "660 60 96" "16 22 32 44" "1 2 4 8 16" 44 2
-
+run_sweep.sh hc_drivaer_s "220 20 32" "16 22 32 44" "1 2 4 8 16" 44 2 hc44rs
+run_sweep.sh hc_drivaer_m "440 40 64" "16 22 32 44" "1 2 4 8 16" 44 2 hc44rs
+run_sweep.sh hc_drivaer_l "660 60 96" "16 22 32 44" "1 2 4 8 16" 44 2 hc44rs
--- run_sweep.sh
+++ run_sweep.sh
@@ -6,6 +6,7 @@ ppn_list=$3
 node_list=$4
 ncpus=$5
 nnuma=$6
+slot_type=$7
 
 source $HOME/OpenFOAM/setenv.sh
 
@@ -13,15 +14,15 @@ mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $mydir
 
-mesh_id=$(qsub -N "mesh_${name}" -l select=1:ncpus=${ncpus}:mpiprocs=$(( $ncpus / 2 )),place=scatter:excl -- $(pwd)/submit_mesh.sh $name "$size" $nnuma | cut -d'.' -f1)
+mesh_id=$(qsub -N "mesh_${name}" -l select=1:slot_type=${slot_type}:ncpus=${ncpus}:mpiprocs=$(( $ncpus / 2 )),place=scatter:excl -- $(pwd)/submit_mesh.sh $name "$size" $nnuma | cut -d'.' -f1)
 
 for ppn in $ppn_list; do
 
     for nodes in $node_list; do
 
-        decompose_id=$(qsub -N "decompose_${name}_${nodes}x${ppn}" -l select=1:ncpus=${ncpus}:mpiprocs=$(( $ncpus / 2 )),place=scatter:excl -W depend=afterok:${mesh_id} -- $(pwd)/submit_decompose.sh $name $nodes $ppn | cut -d'.' -f1)
-        presolver_id=$(qsub -N "presolver_${name}_${nodes}x${ppn}" -l select=${nodes}:ncpus=${ncpus}:mpiprocs=${ppn},place=scatter:excl -W depend=afterok:${decompose_id} -- $(pwd)/submit_presolver.sh $name $nnuma | cut -d'.' -f1)
-        qsub -N "solver_${name}_${nodes}x${ppn}" -l select=${nodes}:ncpus=${ncpus}:mpiprocs=${ppn},place=scatter:excl -W depend=afterok:${presolver_id} -- $(pwd)/submit_solver.sh $name $nnuma
+        decompose_id=$(qsub -N "decompose_${name}_${nodes}x${ppn}" -l select=1:slot_type=${slot_type}:ncpus=${ncpus}:mpiprocs=$(( $ncpus / 2 )),place=scatter:excl -W depend=afterok:${mesh_id} -- $(pwd)/submit_decompose.sh $name $nodes $ppn | cut -d'.' -f1)
+        presolver_id=$(qsub -N "presolver_${name}_${nodes}x${ppn}" -l select=${nodes}:slot_type=${slot_type}:ncpus=${ncpus}:mpiprocs=${ppn},place=scatter:excl -W depend=afterok:${decompose_id} -- $(pwd)/submit_presolver.sh $name $nnuma | cut -d'.' -f1)
+        qsub -N "solver_${name}_${nodes}x${ppn}" -l select=${nodes}:slot_type=${slot_type}:ncpus=${ncpus}:mpiprocs=${ppn},place=scatter:excl -W depend=afterok:${presolver_id} -- $(pwd)/submit_solver.sh $name $nnuma
 
     done
 
